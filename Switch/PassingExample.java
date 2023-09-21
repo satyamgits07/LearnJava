3fa56c771bf54c61be40ab9d577aaa4a13073aba
Satyam Jain <129887268+satyamgits07@users.noreply.github.com>
2023-09-22 03:02:54 +0530

    Add PassingExample.java
    Initial commit: Add PassingExample.java
    
    This commit contains the initial code for the Java program in PassingExample.java. The program prints a greeting message to the console.
    
    # FirstCommit

---
+++ Switch/PassingExample.java
@@ -0,0 +1,9 @@
+public class PassingExample {
+    public static void main(String[] args) {
+        String name =" Satyam jain";
+        greet(name);
+    }
+    static void greet(String naam){
+        System.out.println(naam);
+    }
+}
