f6e844e02078aa0ef4f976bbd7e25feef8ca8c7a
Satyam Jain <129887268+satyamgits07@users.noreply.github.com>
2023-09-24 15:10:10 +0530

    Delete Switch/PassingExample.java

--- Switch/PassingExample.java
+++
@@ -1,9 +0,0 @@
-public class PassingExample {
-    public static void main(String[] args) {
-        String name =" Satyam jain";
-        greet(name);
-    }
-    static void greet(String naam){
-        System.out.println(naam);
-    }
-}
