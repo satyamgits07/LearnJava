c87749b1c9e3c4691bbb9d84009a307adb196ef3
Satyam Jain <129887268+satyamgits07@users.noreply.github.com>
2023-11-01 02:30:09 +0530

    Reverse the array using the swap function

--- Arrays/Swap.java
+++ Arrays/Swap.java
@@ -3,14 +3,26 @@ import java.util.*;
 public class Swap {
     public static void main(String[] args) {
         int[] arr = {1, 2, 23, 8, 18};
-        swap(arr, 1, 3);
+        //swap(arr, 1, 3);
+        Reverse(arr);
 
         System.out.println(Arrays.toString(arr));
     }
+    static void Reverse(int[] arr){
+        int index1=0;
+        int index2=arr.length-1;
 
-    static void swap(int[] arr, int index1, int index2){
-        int temp= arr[index1];
-        arr[index1] = arr[index2];
-        arr[index2] = temp;
+        while(index1<index2){
+            //swap the elements at start and end of array
+            swap(arr, index1, index2);
+            index1++;
+            index2--;
+        }
+    }
+
+    static void swap(int[] arr, int start, int end){
+        int temp= arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
     }
 }
